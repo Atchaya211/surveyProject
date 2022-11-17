837fa5a568b2f7707840e9d4375657d090d3e96e
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2022-11-17 16:09:09 +0530

    Project changes

--- Project1/app.py
+++ Project1/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask import app
+from flask import Flask, app, redirect, render_template, request, url_for
+
 app = Flask(__name__)
 
 
 # Route for handling the login page logic
-@app.route('/login', methods=['get', 'post'])
+@app.route('/', methods=['get', 'post'])
 def login():
     error = None
     if request.method == 'post':
         if request.form['mailId'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
-            return redirect(url_for('home'))
+            
         else:
-            return redirect(url_for("/Project1/templates/home"))
+            return redirect("/home")
     return render_template('index.html', error=error)
+
+
+@app.route("/home", methods=['get','post'])
+def home():
+    error = None
+    return render_template('home.html', error=error)
+
+
+
+
 
 
 
