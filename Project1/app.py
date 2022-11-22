78b61be966adb21a2aebaa99ef62ab40d113fc7d
Tulipraaj <113587246+Tulipraaj@users.noreply.github.com>
2022-11-22 14:58:17 +0530

    project changes display

--- Project1/app.py
+++ Project1/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, app, redirect, render_template, request, url_for
-
+import plotly.graph_objects as go
+import json
+import plotly
+import pandas as pd
+import numpy as np
 app = Flask(__name__)
 
 
@@ -21,12 +25,30 @@ def home():
     error = None
     return render_template('home.html', error=error)
 
+@app.route("/display", methods=['get','post'])
+def display():
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+    z = z_data.values
+    sh_0, sh_1 = z.shape
+    x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+    fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                    width=500, height=500,
+                    margin=dict(l=65, r=50, b=65, t=90))
+    graphJson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('display.html', graphJson=graphJson)
+
+
 @app.route("/input", methods=['get','post'])
 def input():
     error = None
     return render_template('input.html', error=error)
 
 
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
